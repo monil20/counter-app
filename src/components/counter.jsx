a30f354ebef03cea581b240de86754196ef84e4d
Monil Andharia <monil.andharia@crestdatasys.com>
2020-05-05 14:22:31 +0530

    Added navbar

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -13,7 +13,7 @@ class Counter extends Component {
         </button>
         <button
           onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="btn btn-danger btn-sm m-2"
+          className="btn btn-danger btn-sm"
         >
           Delete
         </button>
@@ -22,7 +22,7 @@ class Counter extends Component {
   }
 
   getBadgeClasses() {
-    let spanClassName = "badge m-2 badge-";
+    let spanClassName = "badge badge-";
     spanClassName += this.props.counter.value === 0 ? "warning" : "primary";
     return spanClassName;
   }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,52 +2,21 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 1 },
-      { id: 2, value: 2 },
-      { id: 3, value: 3 },
-    ],
-  };
-
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter(
-      (counter) => counter.id !== counterId
-    );
-    this.setState({ counters });
-  };
-
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map((counter) => {
-      counter.value = 0;
-      return counter;
-    });
-    this.setState({ counters });
-  };
-
   render() {
     return (
       <div>
         <button
           className="btn btn-sm btn-primary m-2"
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
         >
           Reset
         </button>
-        {this.state.counters.map((counter) => (
+        {this.props.counters.map((counter) => (
           <Counter
             key={counter.id}
             counter={counter}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
           />
         ))}
       </div>
